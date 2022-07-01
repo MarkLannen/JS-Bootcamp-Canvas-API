15eef97b01cb31bed972f9699ceb37b8cd444b6d
Mark Lannen <mark.lannen@mso.umt.edu>
2022-07-01 09:10:56 -0600

    Create canvas, add borders and create initial square

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
 const engine = Engine.create();
 const { world } = engine;
-const render = Render.create({
+const render = Render.create( {
   element: document.body,
   engine: engine,
   options: {
@@ -10,10 +10,21 @@ const render = Render.create({
     height: 600
   }
 });
+
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+World.add(world, MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas)
+}))
+
+// Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
+  Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
+  Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
+  Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
+];
+World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
