2ee5c0fd0c914d831bdfa6874de4f7a83a8edfa2
Mark Lannen <mark.lannen@mso.umt.edu>
2022-07-01 11:47:28 -0600

    set up 2d arrays for grid and vertical and horizontal borders

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const cells = 3;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -9,7 +10,7 @@ const render = Render.create( {
   element: document.body,
   engine: engine,
   options: {
-    wireframes: false,
+    wireframes: true,
     width,
     height
   }
@@ -18,32 +19,43 @@ const render = Render.create( {
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-  mouse: Mouse.create(render.canvas)
-}))
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
-  Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
-  Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
-  Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
+  Bodies.rectangle(width/2, 0, width, 40, {isStatic: true}),
+  Bodies.rectangle(width/2, height, width, 40, {isStatic: true}),
+  Bodies.rectangle(0, height/2, 40, height, {isStatic: true}),
+  Bodies.rectangle(height, height/2, 40, 600, {isStatic: true})
 ];
 World.add(world, walls);
 
-// Random Shapes 
+// Maze generation
 
-for (let i = 0; i < 50; i++) {
-  if(Math.random() > 0.5) {
-    World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-  } else {
-    World.add(
-      world, 
-      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-        render: {
-          fillStyle: 'red'
-        }
-      })
-    );
-  }
-}
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false)
+)
+
+const verticals = Array(cells).fill(null).map(() => Array(cells-1).fill(false));
+
+const horizontals = Array(cells-1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const setpThroughCell = (row, column) => {
+  // If I have visited the cell at [row,column] then return
+
+  // Mark this cell as being visited
+
+  // Assemble randomly ordered list of neighbors
+
+  // For each neighbor...
+
+  // See if that neighnor is out of bounds
+
+  // If we have visited that neighbor, continue to next neighbor
+
+  // Remove a wall from either horizontals or verticals
+
+  // Visit that next cell
+};
+
+setpThroughCell(startRow, startColumn);
